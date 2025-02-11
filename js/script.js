c14965f01c6907a8915ba0ece9a5280a7ff073f9
Marcelino Budi Prakasya <66460230+marcelinobudi@users.noreply.github.com>
2025-02-11 12:21:01 +0700

    Memperbaharui sistem deteksi
    Mendeteksi sistem melalui API

--- js/script.js
+++ js/script.js
@@ -1,26 +1,27 @@
 let imgBBAPI = "69916af40b37867cced6a452843ec457";
+let apiLink = "https://6625-35-197-68-141.ngrok-free.app/"
 
 async function uploadImage(file) {
   
   if(!file){
     return;
   }
+  let imgUrl = ""
 
-  const formData = new FormData();
-  formData.append("image", file);
+  const formDataUploadImage = new FormData();
+  formDataUploadImage.append("image", file);
 
-  console.log(formData)
 
   try{
     const response = await fetch("https://api.imgbb.com/1/upload?expiration=600&key=" + imgBBAPI, {
       method: "POST",
-      body: formData
+      body: formDataUploadImage
     });
     
     const data = await response.json();
 
     if(data.success){
-      const imageUrl = data.data.url;
+      imageUrl = data.data.url;
       console.log(imageUrl);
     } else{
       alert("Gagal mengunggah gambar");
@@ -29,6 +30,22 @@ async function uploadImage(file) {
   catch(error){
     console.error("Error: ", error);
     alert("Terjadi kesalahan saat mengunggah");
+  }
+
+  const formDataDetectImage = new FormData();
+  formDataDetectImage.append("image_url", imgUrl)
+  try{
+    const response = await fetch(apiLink + "detect-deepfake", {
+      method: "POST",
+      body: formDataDetectImage
+    })
+
+    const data = await response.json();
+    document.getElementById("hasil") = data
+  }
+  catch(error){
+    console.error("Error: ", error);
+    alert("Terjadi kesalahan saat mendeteksi");
   }
 }
 

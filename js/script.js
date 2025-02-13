7f55750ee285f15e6dbcf41217dc8d5b7a3dcaaf
Marcelino Budi Prakasya <66460230+marcelinobudi@users.noreply.github.com>
2025-02-13 20:03:05 +0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,54 +1,74 @@
 let imgBBAPI = "69916af40b37867cced6a452843ec457";
-let apiLink = "https://6625-35-197-68-141.ngrok-free.app/"
+let apiLink = "https://7a56-34-19-112-210.ngrok-free.app/"
 
-async function uploadImage(file) {
+async function uploadImageToIMGBB(file) {
   
   if(!file){
     return;
   }
+
   let imgUrl = ""
+  let dataResponse = {}
+  const formData = new FormData();
 
-  const formDataUploadImage = new FormData();
-  formDataUploadImage.append("image", file);
 
+  formData.append("image", file);
 
   try{
     const response = await fetch("https://api.imgbb.com/1/upload?expiration=600&key=" + imgBBAPI, {
       method: "POST",
-      body: formDataUploadImage
+      body: formData
     });
-    
-    const data = await response.json();
 
-    if(data.success){
-      imageUrl = data.data.url;
-      console.log(imageUrl);
-    } else{
-      alert("Gagal mengunggah gambar");
-    }
+    dataResponse = await response.json();
   }
   catch(error){
     console.error("Error: ", error);
     alert("Terjadi kesalahan saat mengunggah");
+    return null;
   }
 
-  const formDataDetectImage = new FormData();
-  formDataDetectImage.append("image_url", imgUrl)
+  if(dataResponse.success){
+    imgUrl = dataResponse.data.url;
+  } else{
+    alert("Gagal mengunggah gambar");
+    return null;
+  }
+
+  return imgUrl;
+}
+
+async function detectImage(url){
+  
+  let dataResponse = ""
+  const formData = new FormData();
+  formData.append("image_url", url);
+
   try{
     const response = await fetch(apiLink + "detect-deepfake", {
       method: "POST",
-      body: formDataDetectImage
+      body: formData
     })
 
-    const data = await response.json();
-    document.getElementById("hasil") = data
+    dataResponse = await response.json();
   }
   catch(error){
     console.error("Error: ", error);
     alert("Terjadi kesalahan saat mendeteksi");
   }
+  
+  return dataResponse;
 }
 
-document.getElementById('fileInput').addEventListener('change', function(){
-  uploadImage(this.files[0])
+
+document.getElementById('fileInput').addEventListener('change', async function(){
+  let imgUrl = await uploadImageToIMGBB(this.files[0]);
+  let detectionResult;
+
+  if(imgUrl != null){
+    detectionResult = await detectImage(imgUrl);
+    console.log("Detection result ", detectionResult)
+    document.getElementById("hasil") = detectionResult
+  }
+
 })
